846c8fe397b0b715efbcebdba8db4637026f20da
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-05 14:09:56 -0800

    Update EfficientFrontierModel and rename riskmodeling.py to riskmanagement.py

--- riskmanagement.py
+++ riskmanagement.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-class EfficientFrontier:
+class EfficientFrontierModel:
     def __init__(self, adjusted_close: pd.DataFrame):
         self.returns: pd.DataFrame= adjusted_close.pct_change()
         self.mean_returns = self.returns.mean()

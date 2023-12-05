871b97f30fa6017225fd61beda12ede70975a83c
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-05 14:05:30 -0800

    Update riskmodeling.py EfficientFrontier

--- riskmodeling.py
+++ riskmodeling.py
@@ -1,5 +1,7 @@
-from typing import List
+import pandas as pd
 
 class EfficientFrontier:
-    def __init__(self):
-        pass
+    def __init__(self, adjusted_close: pd.DataFrame):
+        self.returns: pd.DataFrame= adjusted_close.pct_change()
+        self.mean_returns = self.returns.mean()
+        self.covariance_matric = self.returns.cov()

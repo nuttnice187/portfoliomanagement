23813b483a69e9818428a6e54c69727b2e60f2a1
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-05 14:34:54 -0800

    Update riskmanagement.py EfficientFrontierModel

--- riskmanagement.py
+++ riskmanagement.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import numpy as np
 
 class EfficientFrontierModel:
+    percent_change: pd.DataFrame
     mean_returns: pd.Series
     covariance_matrix: pd.DataFrame
+    returns: float
+    sigma: float
     def __init__(self, adjusted_close: pd.DataFrame):
-        returns = adjusted_close.pct_change()
-        self.mean_returns = returns.mean()
-        self.covariance_matrix = returns.cov()
+        self.percent_change = adjusted_close.pct_change()
+        self.mean_returns = percent_change.mean()
+        self.covariance_matrix = percent_change.cov()
+    def get_portfolio_performance(self, weights, trading_days: int=252):
+        self.returns = np.sum(self.mean_returns*weights)*trading_days
+        self.sigma = np.sqrt(np.dot(weights.T, np.dot(self.covariance_matrix, weights))
+            )*np.sqrt(trading_days)

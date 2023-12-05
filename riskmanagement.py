25dbd8b17c6a2020e66122b2e93d2ff0b4a3254b
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-05 14:21:09 -0800

    Update riskmanagement.py

--- riskmanagement.py
+++ riskmanagement.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
 class EfficientFrontierModel:
-    returns: pd.DataFrame
     mean_returns: pd.Series
     covariance_matrix: pd.DataFrame
     def __init__(self, adjusted_close: pd.DataFrame):
-        self.returns = adjusted_close.pct_change()
-        self.mean_returns = self.returns.mean()
-        self.covariance_matrix = self.returns.cov()
+        returns = adjusted_close.pct_change()
+        self.mean_returns = returns.mean()
+        self.covariance_matrix = returns.cov()

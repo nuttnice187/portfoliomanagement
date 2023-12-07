119e6c40ad4f92118fadbc19f75bbbf6ab162d8a
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-07 14:08:00 -0800

    Update risk.py __get_optimal_portfolio

--- risk.py
+++ risk.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.typing as npt
 import pandas as pd
 from typing import Dict, List, Tuple, Union
-from scipy.optimize import minimize
+from scipy.optimize import minimize, OptimizeResult
 
 def get_portfolio_performance(weights: npt.NDArray[np.float64],
     mean_returns: pd.Series, cov_matrix: pd.DataFrame, trading_days: int
@@ -25,6 +25,7 @@ class EfficientFrontierModel:
     risk_free_rate: float
     trading_days: int
     asset_len: int
+    max_sharpe_ratio_portfolio: OptimizeResult
     def __init__(self, adjusted_close: pd.DataFrame, risk_free_rate: float=0.04
         ) -> None:
         self.trading_days, self.asset_len = adjusted_close.shape
@@ -32,15 +33,15 @@ class EfficientFrontierModel:
         self.mean_returns = percent_change.mean()
         self.cov_matrix = percent_change.cov()
         self.risk_free_rate = risk_free_rate
-    def get_optimal_portfolio(self, weight_limit: Tuple[float, float]=(0, 1)
+        self.__get_optimal_portfolio()
+    def __get_optimal_portfolio(self, weight_limit: Tuple[float, float]=(0, 1)
         ) -> None:
         constraints: Dict[str, Union[str, function]]= {"type": 'eq',
             "fun": lambda x: np.sum(x) - 1}
         bounds: Tuple[Tuple[float, float]]= tuple(
             weight_limit for i in range(self.asset_len))
         initial_weights: List[float]= self.asset_len*[1/self.asset_len]
-        res = minimize(get_neg_sharpe_ratio, initial_weights, args=(
-                self.mean_returns, self.cov_matrix, self.trading_days,
-                self.risk_free_rate),
+        self.max_sharpe_ratio_portfolio = minimize(get_neg_sharpe_ratio,
+            initial_weights, args=(self.mean_returns, self.cov_matrix,
+                self.trading_days, self.risk_free_rate),
             method='SLSQP', bounds=bounds, constraints=constraints)
-        return res

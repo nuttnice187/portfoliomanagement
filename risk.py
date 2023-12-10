7f659d67fc4fbecf71dc584ffcc9080247126743
Phillip Andrew Raywood <thephillipraywood@gmail.com>
2023-12-09 16:17:31 -0800

    Update risk.py Update __init__ __get_optimal_portfolio

--- risk.py
+++ risk.py
@@ -35,14 +35,16 @@ class EfficientFrontierModel:
     asset_len: int
     max_sharpe_ratio_portfolio: OptimizeResult
     min_risk_portfolio: OptimizeResult
-    def __init__(self, adjusted_close: pd.DataFrame, risk_free_rate: float=0.04
-        ) -> None:
+    bound: Tuple[float, float]
+    def __init__(self, adjusted_close: pd.DataFrame, risk_free_rate: float=0.04,
+        bound: Tuple[float, float]=(0, 1)) -> None:
         self.symbols = adjusted_close.columns
         self.trading_days, self.asset_len = adjusted_close.shape
         percent_change = adjusted_close.pct_change()
         self.mean_returns = percent_change.mean()
         self.cov_matrix = percent_change.cov()
         self.risk_free_rate = risk_free_rate
+        self.bound = bound
         self.max_sharpe_ratio_portfolio = self.__get_optimal_portfolio(
             *(get_neg_sharpe_ratio, self.mean_returns, self.cov_matrix,
                 self.trading_days, self.risk_free_rate))
@@ -70,12 +72,11 @@ class EfficientFrontierModel:
             Callable[[NDArray[np.float64], pd.DataFrame, int], float],
             Callable[[NDArray[np.float64], pd.Series, pd.DataFrame, int, float],
                 float]],
-        *args: Union[pd.Series, pd.DataFrame, int, float],
-        weight_limit: Tuple[float, float]=(0, 1)) -> OptimizeResult:
+        *args: Union[pd.Series, pd.DataFrame, int, float]) -> OptimizeResult:
         constraints: Dict[str, Union[str, function]]= {"type": 'eq',
             "fun": lambda x: np.sum(x) - 1}
         bounds: Tuple[Tuple[float, float]]= tuple(
-            weight_limit for i in range(self.asset_len))
+            self.bound for i in range(self.asset_len))
         initial_weights: List[float]= self.asset_len*[1/self.asset_len]
         return minimize(fun, initial_weights, args=args, method='SLSQP',
             bounds=bounds, constraints=constraints)
